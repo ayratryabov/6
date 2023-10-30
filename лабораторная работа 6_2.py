075f3db85958e650190cdad462ce4548075e4788
ayratryabov <127486087+ayratryabov@users.noreply.github.com>
2023-10-30 15:54:56 +0300

    Update лабораторная работа 6_2.py

--- лабораторная работа 6_2.py
+++ лабораторная работа 6_2.py
@@ -1,15 +1,22 @@
-#усложнить написанную программу, введя по своему усмотрению в условие минимум одно ограничение на характеристики объектов и целевую функцию для оптимизации решения.
-
 #black blue white
 #pidj
 from random import *
+print('У юноши вещи разделены на три части по цветам,черный,синий и белый')
 JAKET = []
 
+def input_1():
+    global lot_jak
+    lot_jak = int(input('Введите кол-во пиджаков: '))
+    if lot_jak < 0:
+        print('Введите положительное значение ')
+        input_1()
+input_1()
 def jaket():
-    lot_jak ,quest_Jak = int(input('Введите кол-во пиджаков: ')),int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
+    
+    quest_Jak = int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
     if quest_Jak == 0 or quest_Jak > 3:
         print('Вы ввели неккоректную цифру,попробуйте еще раз!')
-        int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
+        jaket()
     
    
     black_jak =  lot_jak//3 #1
@@ -38,17 +45,24 @@ def jaket():
             for i in range((black_jak+blue_jak)+1,black_jak+blue_jak+white_jak+1):
                 JAKET.append(f'Пиджак{i}')
     
-    
-TIES = []       
+jaket()    
+TIES = []  
+def input_2():
+    global lot_tie
+    lot_tie = int(input('Введите кол-во галстуков: '))
+    if lot_tie < 0:
+        print('Введите положительное значение ')
+        input_2()
+input_2()     
 #galst
 def ties():
-    lot_tie ,quest_Tie = int(input('Введите кол-во галстуков: ')),int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
+    quest_Tie = int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
     black_tie =  lot_tie//3 #1
     blue_tie = (lot_tie - black_tie)//2 #2
     white_tie = (lot_tie - black_tie)-blue_tie  #3
     if quest_Tie == 0 or quest_Tie > 3:
         print('Вы ввели неккоректную цифру,попробуйте еще раз!')
-        int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
+        ties()
     if lot_tie == 0:
         TIES.append('Галстуков нет')
         
@@ -72,15 +86,22 @@ def ties():
         else:
             for i in range((black_tie+blue_tie)+1,black_tie+blue_tie+white_tie+1):
                 TIES.append(f'Галстук{i}')
-    
+ties()    
 TROU = []
+def input_3():
+    global lot_t
+    lot_t = int(input('Введите кол-во брюк: '))
+    if lot_t < 0:
+        print('Введите положительное значение ')
+        input_3()
+input_3() 
 #bruki
 def trousers():
     
-    lot_t ,quest_T = int(input('Введите кол-во брюк : ')),int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
+    quest_T = int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
     if quest_T == 0 or quest_T > 3:
         print('Вы ввели неккоректную цифру,попробуйте еще раз!')
-        int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
+        trousers()
     if lot_t == 0:
         TROU.append('брюк нет')
    
@@ -107,16 +128,22 @@ def trousers():
         else:
             for i in range((black_t+blue_t)+1,black_t+blue_t+white_t+1):
                 TROU.append(f'Брюки{i}')
-    
- 
+trousers()    
+def input_4():
+    global lot_tie
+    lot_tie = int(input('Введите кол-во рубашек: '))
+    if lot_tie < 0:
+        print('Введите положительное значение ')
+        input_4()
+input_4() 
 SHIR = []    
 #rubashka
 def shirts():
     
-    lot_t ,quest_T = int(input('Введите кол-во рубашек: ')),int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
+    quest_T = int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
     if quest_T == 0 or quest_T > 3:
         print('Вы ввели неккоректную цифру,попробуйте еще раз!')
-        int(input("Выберите цвет: 1-черный,2-синий,3-белый "))
+        shirts()
    
     if lot_t == 0:
         SHIR.append('Рубашек у юноши нет')
@@ -143,7 +170,7 @@ def shirts():
         else:
             for i in range((black_t+blue_t)+1,black_t+blue_t+white_t+1):
                 SHIR.append(f'Рубашка{i}')
-    
+shirts()   
 ALL = []
 def all_costum():
     
@@ -159,13 +186,7 @@ def all_costum():
                     ALL.append(ol)
                     ol = []
     
-    
 
-print('У юноши вещи разделены на три части по цветам,черный,синий и белый')
-jaket()
-ties()
-trousers()
-shirts()
 all_costum()
 
 
